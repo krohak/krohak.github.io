efae8bc917e43f9606d3549b8318478eea754431
krohak <rohaksinghal14@gmail.com>
2021-06-28 20:53:27 +0800

    favicon fix

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import {
 import { createBrowserHistory } from "history";
 import { Projects } from './Projects';
 
-//TODO: Fix Favicon 
 ReactDOM.render(
   <React.StrictMode>
     <Router history={createBrowserHistory()}>
