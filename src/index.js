55327e10829e9fdcbeaf31542148bd5b51e21891
krohak <rohaksinghal14@gmail.com>
2021-06-29 11:50:12 +0800

    fix router on gh pages

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {
-  BrowserRouter as Router,
+  Router,
   Switch,
   Route,
 } from "react-router-dom";
